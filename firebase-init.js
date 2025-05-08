fd44f0690eb033b78e83e225cdfce1f2edf90f92
ISUCKAZZ <168322041+ISUCKAZZ@users.noreply.github.com>
2025-05-07 21:59:59 -0400

    fix

--- firebase-init.js
+++ firebase-init.js
@@ -1,13 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA7TO4TA7fe6lPmcVkKpsnmCjMC93kyD0w",
-    authDomain: "laggyworld-be6e4.firebaseapp.com",
-    projectId: "laggyworld-be6e4",
-    storageBucket: "laggyworld-be6e4.firebasestorage.app",
-    messagingSenderId: "198058418554",
-    appId: "1:198058418554:web:ac721922e3f1724243f6c3"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  
+  apiKey: "AIzaSyA7TO4TA7fe6lPmcVkKpsnmCjMC93kyD0w",
+  authDomain: "laggyworld-be6e4.firebaseapp.com",
+  projectId: "laggyworld-be6e4",
+  storageBucket: "laggyworld-be6e4.firebasestorage.app",
+  messagingSenderId: "198058418554",
+  appId: "1:198058418554:web:ac721922e3f1724243f6c3"
+};
+
+const app = initializeApp(firebaseConfig);
